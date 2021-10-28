c2a2ee72c2f89bc4d03705341bfb6c398d66c650
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-10-28 16:38:03 +0300

    Добавлены новые экраны
    Добавлены экраны, изменён дизайн и структура приложения

--- client.py
+++ client.py
@@ -2,52 +2,61 @@ import socket
 import threading
 
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivy.core.window import Window
+from kivy.config import Config
 
-Window.size = (500, 1000)
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+
+Window.size = (480, 853)
 Window.title = 'Messenger'
-
-
-class ChatScreen(FloatLayout):
-    def __init__(self, ip: str, port: int):
-        super().__init__()
-
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ip = ip
-        self.port = port
-
-    def connect_socket(self):
-        try:
-            self.s.connect((self.ip, self.port))
-            threading.Thread(target=self.listen, args=(self.s,), daemon=True).start()
-        except Exception:
-            self.chat_widget.text += 'Не удалось подключиться к серверу \n'
-
-    def send_message(self):
-        current_text = self.chat_input_widget.text
-        if current_text == '':
-            return
-        self.chat_widget.text += '[Вы] ' + current_text + '\n'
-        self.s.send(('[Собеседник] ' + current_text).encode('utf-8'))
-        self.chat_input_widget.text = ''
-
-    def listen(self, instance):
-        while True:
-            msg = self.s.recv(1024)
-            self.chat_widget.text += msg.decode('utf-8') + '\n'
+Window.clearcolor = (.85, .85, .85, 1)
 
 
 class ClientApp(App):
+    class ScreenManagement(ScreenManager):
+        pass
+
+    class StartScreen(Screen):
+        pass
+
+    class RegistrationScreen(Screen):
+        pass
+
+    class LoginScreen(Screen):
+        pass
+
+    class ChatScreen(Screen):
+        def connect_socket(self):
+            try:
+                self.s.connect((self.ip, self.port))
+                threading.Thread(target=self.listen, args=(self.s,), daemon=True).start()
+            except Exception:
+                self.chat_widget.text += 'Не удалось подключиться к серверу \n'
+
+        def send_message(self):
+            current_text = self.chat_input_widget.text
+            if current_text == '':
+                return
+            self.chat_widget.text += '[Вы] ' + current_text + '\n'
+            self.s.send(('[Собеседник] ' + current_text).encode('utf-8'))
+            self.chat_input_widget.text = ''
+
+        def listen(self, instance):
+            while True:
+                msg = self.s.recv(1024)
+                self.chat_widget.text += msg.decode('utf-8') + '\n'
 
     def __init__(self, ip: str, port: int):
         super().__init__()
         self.ip = ip
         self.port = port
+        self.build_kv = Builder.load_file('client.kv')
 
     def build(self):
-        return ChatScreen(self.ip, self.port)
+        return self.build_kv
 
 
 if __name__ == '__main__':

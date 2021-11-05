2f1764cc95004b44c1716ed36303eaa4989d8a97
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-11-05 14:38:30 +0300

    Добавлен процесс проверки данных
    Для зарегистрированных пользователей проверяется информация в базе данных, для новых пользователей добавляется информация в базу данных

--- server.py
+++ server.py
@@ -2,12 +2,15 @@ import time
 import socket
 import threading
 
+import database
+
 
 class Server:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.members = []
+        self.adresa = []
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.ip, self.port))
@@ -20,14 +23,28 @@ class Server:
             client, address = self.server.accept()
             if client not in self.members:
                 self.members.append(client)
-                threading.Thread(target=self.message_handler, args=(client,)).start()
-                client.send('Успешное подключение к чату!'.encode('utf-8'))
+                self.adresa.append(address)
+                print(client)
+                threading.Thread(target=self.check_user_data, args=(client,)).start()
+                # client.send('Успешное подключение к чату!'.encode('utf-8'))
             time.sleep(1)
+
+    def check_user_data(self, client_socket):
+        data = client_socket.recv(1024)
+        if data[0]== 'l':
+            accept = database.check_user(data[1::])
+            client_socket.send(accept.encode('utf-8'))
+            if accept:
+                threading.Thread(target=self.message_handler, args=(client_socket,)).start()
+            else:
+                threading.Thread(target=self.connect, args=(client_socket,)).start()
+        if data[0] == 'r':
+            database.add_user(data[1::])
+            threading.Thread(target=self.message_handler, args=(client_socket,)).start()
 
     def message_handler(self, client_socket):
         while True:
             message = client_socket.recv(1024)
-            print(message)
 
             for client in self.members:
                 if client != client_socket:

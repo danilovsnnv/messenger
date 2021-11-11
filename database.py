baed69eacb3a32b6e71aa6bdcc71d6001999cb33
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-11-11 22:36:31 +0300

    Изменены функции добавления и поиска пользователя
    Функции принимают имя пользователя и строку из логина и пароля, функция добавления пользователя возвращает логическое значение

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, Integer, String, Column, exists
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-BDPATH = 'base.db'
+BDPATH = 'dbase.db'
 
 base = declarative_base()
 engine = create_engine('sqlite:///' + BDPATH + '?check_same_thread=False', echo=True)
@@ -15,9 +15,9 @@ class Users(base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    user_name = Column(String(50), nullable=True)
-    user_hash = Column(Integer, nullable=False, unique=True)
-    user_open_key = Column(Integer, nullable=True, unique=True)
+    user_name = Column(String(50), nullable=True, unique=True)
+    user_hash = Column(Integer, nullable=False, unique=False)
+    user_open_key = Column(Integer, nullable=True)
 
 
 if not os.path.exists(BDPATH):
@@ -25,16 +25,14 @@ if not os.path.exists(BDPATH):
     session.commit()
 
 
-def add_user(data):
-    user_hash = hash(data)
-    if not has_user(user_hash):
-        session.add(Users(user_hash=user_hash))
+def add_user(username: str, data: str) -> bool:
+    user_exists = has_user(username, data)
+    if not user_exists:
+        session.add(Users(user_name=username, user_hash=hash(data)))
         session.commit()
+    return not user_exists
 
 
-def has_user(user_hash: int) -> bool:
-    return session.query(exists().where(Users.user_hash == user_hash)).scalar()
-
-
-def check_user(data: str):
-    return session.query(exists().where(Users.user_hash == hash(data))).scalar()
+def has_user(user_name: str, data: str) -> bool:
+    return session.query(exists().where(Users.user_name == user_name and
+                                        Users.user_hash == hash(data))).scalar()

575b3ceab4413b2dd64326ae7c0505a3a8241f6f
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-11-12 02:02:20 +0300

    Модуль для проверки ввода данных
    Функции служат для проверки корректности введённого логина и пароля

--- form_check.py
+++ form_check.py
@@ -1,15 +1,10 @@
 import re
 def check_login(log, pattern = r'[a-zA-Z][a-zA-Z0-9]{,31}$'):
   # Паттерн по умолчанию проверяет, что логин написан на английском, начинается с буквы, содержит не более 32 символов и не содержит спецсимволов
-  if re.match(pattern, log):
-    return True
-  else:
-    return False
+  return re.match(pattern, log):
+    
 
 def check_passpord(pwd, pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$'):
   # Паттерг по умолчанию задает пароль из не менее восьми символов, содержащий английские буквы в разных регистрах и цифры
-  if re.match(pattern, pwd):
-    return True
-  else:
-    return False
-
+  return re.match(pattern, pwd):
+   

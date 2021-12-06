54d53bbeb8b20affd6c0c95190b565ebf0ebe826
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-12-06 23:33:13 +0300

    Исправлены ошибки в коде

--- client.py
+++ client.py
@@ -17,7 +17,10 @@ Window.size = (480, 853)
 Window.title = 'Messenger'
 Window.clearcolor = (.85, .85, .85, 1)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = '127.0.0.1'
+PORT = 6555
+USERNAME = ''
 
 
 class ScreenManagement(ScreenManager):
@@ -25,6 +28,16 @@ class ScreenManagement(ScreenManager):
 
 
 class StartScreen(Screen):
+    def check_socket(self, next_screen_name: str):
+        global SOCKET
+        try:
+            SOCKET.connect((IP, PORT))
+            self.parent.current = next_screen_name
+        except WindowsError:
+            self.parent.current = 'connection_error'
+
+
+class ConnectionErrorScreen(Screen):
     pass
 
 
@@ -32,14 +45,14 @@ class LoginScreen(Screen):
     def send_user_data(self):
         if form_check.check_login(self.login_widget.text) and form_check.check_password(self.password_widget.text):
             data = 'l' + str(self.login_widget.text) + str(self.password_widget.text)
-            s.send(self.login_widget.text.encode('utf-8'))
-            s.send(data.encode('utf-8'))
+            SOCKET.send(self.login_widget.text.encode('utf-8'))
+            SOCKET.send(data.encode('utf-8'))
             threading.Thread(target=self.accept_handler).start()
         else:
             self.error_label_widget.text = 'Проверьте введённые данные'
 
     def accept_handler(self):
-        accept = str(s.recv(1024))[2:-1]
+        accept = str(SOCKET.recv(1024))[2:-1]
         if accept == 'True':
             self.parent.current = 'chat'
             threading.Thread(target=self.parent.screens[-1].listen).start()
@@ -53,8 +66,8 @@ class RegistrationScreen(Screen):
             if form_check.check_password(self.password1_widget.text):
                 if self.password1_widget.text == self.password2_widget.text:
                     data = 'r' + str(self.login_widget.text) + str(self.password1_widget.text)
-                    s.send(self.login_widget.text.encode('utf-8'))
-                    s.send(data.encode('utf-8'))
+                    SOCKET.send(self.login_widget.text.encode('utf-8'))
+                    SOCKET.send(data.encode('utf-8'))
                     threading.Thread(target=self.accept_handler).start()
                 else:
                     self.error_label_widget.text = 'Введённые пароли не совпадают'
@@ -66,49 +79,43 @@ class RegistrationScreen(Screen):
                                            'содержать не более 32 символов и не содержать спецсимволов'
 
     def accept_handler(self):
-        accept = str(s.recv(1024))[2:-1]
+        accept = str(SOCKET.recv(1024))[2:-1]
         if accept == 'True':
             self.parent.current = 'chat'
         else:
             self.error_label_widget.text = 'Данный пользователь уже существует'
 
 
-class ChatScreen(Screen):
-
+class MessagesScreen(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
 
+
+class ChatScreen(Screen):
     def send_message(self):
         current_text = self.chat_input_widget.text
         if current_text == '':
             return
         self.chat_widget.text += '[Вы] ' + current_text + '\n'
         self.chat_input_widget.text = ''
-        s.send(('[Собеседник] ' + current_text).encode('utf-8'))
+        SOCKET.send(('[Собеседник] ' + current_text).encode('utf-8'))
 
     def listen(self):
         while True:
-            msg = s.recv(1024)
+            msg = SOCKET.recv(1024)
             self.chat_widget.text += msg.decode('utf-8') + '\n'
 
 
 class ClientApp(App):
 
-    def __init__(self, ip: str, port: int):
+    def __init__(self):
         super().__init__()
-        self.ip = ip
-        self.port = port
         self.build_kv = Builder.load_file('client.kv')
-        global s
-        try:
-            s.connect((self.ip, self.port))
-        except Exception as e:
-            print(e)
 
     def build(self):
         return self.build_kv
 
 
 if __name__ == '__main__':
-    client = ClientApp('127.0.0.1', 6555)
+    client = ClientApp()
     client.run()

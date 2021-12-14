547550aed992313e812481366af432e26c64c944
qxygen <52496357+qxygen@users.noreply.github.com>
2021-12-14 19:41:36 +0300

    Update form_check.py

--- form_check.py
+++ form_check.py
@@ -1,10 +1,10 @@
 import re
 def check_login(log, pattern = r'[a-zA-Z][a-zA-Z0-9]{,31}$'):
   # Паттерн по умолчанию проверяет, что логин написан на английском, начинается с буквы, содержит не более 32 символов и не содержит спецсимволов
-  return re.match(pattern, log):
+  return re.match(pattern, log)
     
 
 def check_passpord(pwd, pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$'):
   # Паттерг по умолчанию задает пароль из не менее восьми символов, содержащий английские буквы в разных регистрах и цифры
-  return re.match(pattern, pwd):
+  return re.match(pattern, pwd)
    

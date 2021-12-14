cf49b33b9c47d7889b8846662e4295cabf8adb47
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-12-14 19:04:30 +0300

    Добавлены новые таблицы
    Добавлены таблицы для хранения информации о сообщениях, добавлено хранение и получение открытого ключа для шифрования

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from sqlalchemy import create_engine, Integer, String, Column, exists
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,9 +16,29 @@ class Users(base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    user_name = Column(String(50), nullable=True, unique=True)
-    user_hash = Column(Integer, nullable=False, unique=False)
-    user_open_key = Column(Integer, nullable=True)
+    username = Column(String(50), nullable=True, unique=True)
+    hash = Column(Integer, nullable=False, unique=False)
+    open_key = Column(String, nullable=True)
+
+
+class Messages(base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    user_from = Column(String(50), nullable=False)
+    user_to = Column(String(50), nullable=False)
+    time = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+
+
+class UnreceivedMessages(base):
+    __tablename__ = 'unreserved'
+
+    id = Column(Integer, primary_key=True)
+    user_from = Column(String(50), nullable=False)
+    user_to = Column(String(50), nullable=False)
+    time = Column(String, nullable=False)
+    message = Column(String, nullable=False)
 
 
 if not os.path.exists(BDPATH):
@@ -25,14 +46,78 @@ if not os.path.exists(BDPATH):
     session.commit()
 
 
-def add_user(username: str, data: str) -> bool:
+def add_user(username: str, data: str, key: str) -> bool:
+    """
+    Поиск пользователя в базе данных
+         :param username: Имя пользователя
+         :param data: Строка логин + пароль
+         :param key: Открытый RSA ключ
+
+         :return True: Пользователь существует в базе данных
+         :return False: Пользователя нет или введены неверные данные
+    """
     user_exists = has_user(username, data)
     if not user_exists:
-        session.add(Users(user_name=username, user_hash=hash(data)))
+        session.add(Users(username=username, hash=hash(data), open_key=key))
         session.commit()
     return not user_exists
 
 
-def has_user(user_name: str, data: str) -> bool:
-    return session.query(exists().where(Users.user_name == user_name and
-                                        Users.user_hash == hash(data))).scalar()
+def has_user(username: str, data: str) -> bool:
+    """
+    Поиск пользователя в базе данных
+        :param username: Имя пользователя
+        :param data: Строка логин + пароль
+
+        :return: True: Пользователь существует в базе данных
+        :return: False: Пользователя нет или введены неверные данные
+    """
+    return session.query(exists().where(Users.username == username and
+                                        Users.hash == hash(data))).scalar()
+
+
+def add_message(user_from: str, user_to: str, message: str, time=datetime.datetime.now()):
+    """
+    Добавление информации о сообщении в базу данных
+        :param user_from: Имя пользователя-отправителя
+        :param user_to: Имя пользователя-адресата
+        :param message: Текст сообщения
+        :param time: Время отправки сообщения (по умолчанию текущее время)
+    """
+    session.add(Messages(user_from=user_from, user_to=user_to, time=str(time), message=message))
+    session.commit()
+
+
+def add_unreceived_message(user_from: str, user_to: str, message: str, time=datetime.datetime.now()):
+    """
+    Добавление в базу данных информации о сообщении, которое не дошло до адресата
+        :param user_from: Имя пользователя-отправителя
+        :param user_to: Имя пользователя-адресата
+        :param message: Текст сообщения
+        :param time: Время отправки сообщения (по умолчанию текущее время)
+    """
+    session.add(UnreceivedMessages(user_from=user_from, user_to=user_to, time=str(time), message=message))
+    session.commit()
+
+
+def get_unreceived_message(username: str):
+    """
+    Ищет неполучаенные сообщения для конкретного пользователя, удаляет их и возвращает
+    :param username: Имя пользователя для поиска
+    :return rec: Список сообщений
+    """
+    rec = list(session.query(UnreceivedMessages.user_from, UnreceivedMessages.message).filter(
+        UnreceivedMessages.user_to == username).all())
+    session.query(UnreceivedMessages).filter(UnreceivedMessages.user_to == username).delete(synchronize_session='fetch')
+    session.commit()
+    return rec
+
+
+def get_open_key(username: str) -> str:
+    """
+    Получает открытый ключ по логину пользователя
+    :param username: Имя пользователя
+    :return: Открытый ключ пользователя 
+    """
+    user = session.query(Users).filter_by(Users.username == username).one()
+    return user.open_key

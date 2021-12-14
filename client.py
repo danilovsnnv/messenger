02c5f315b05eb85617ad901536c6b1e944d5f85d
qxygen <52496357+qxygen@users.noreply.github.com>
2021-12-14 23:10:07 +0300

    Update client.py

--- client.py
+++ client.py
@@ -10,6 +10,8 @@ from kivy.config import Config
 
 import form_check
 
+import rsa
+
 
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
@@ -44,15 +46,22 @@ class ConnectionErrorScreen(Screen):
 class LoginScreen(Screen):
     def send_user_data(self):
         if form_check.check_login(self.login_widget.text) and form_check.check_password(self.password_widget.text):
+            pub_key_file = open("pubkey.txt", "r")
+            pubkey_pem = pub_key_file.read().encode("utf-8")
+            pub_key_file.close()
+            SOCKET.send(pubkey_pem)
+            server_pub_key = rsa.PublicKey.load_pkcs1(SOCKET.recv(1024), 'PEM')
             data = 'l' + str(self.login_widget.text) + str(self.password_widget.text)
-            SOCKET.send(self.login_widget.text.encode('utf-8'))
-            SOCKET.send(data.encode('utf-8'))
+            secret_data = rsa.encrypt(data.encode('utf-8'), server_pub_key)
+            secret_login = rsa.encrypt(self.login_widget.text.encode('utf-8'), server_pub_key)
+            SOCKET.send(secret_login)
+            SOCKET.send(secret_data)
             threading.Thread(target=self.accept_handler).start()
         else:
             self.error_label_widget.text = 'Проверьте введённые данные'
 
     def accept_handler(self):
-        accept = str(SOCKET.recv(1024))[2:-1]
+        accept = SOCKET.recv(1024).decode("utf-8")
         if accept == 'True':
             self.parent.current = 'chat'
             threading.Thread(target=self.parent.screens[-1].listen).start()
@@ -65,9 +74,26 @@ class RegistrationScreen(Screen):
         if form_check.check_login(self.login_widget.text):
             if form_check.check_password(self.password1_widget.text):
                 if self.password1_widget.text == self.password2_widget.text:
+                    # Создание ключей и запись их в файл
+                    (pubkey, privkey) = rsa.newkeys(1024)
+                    pubkey_pem = pubkey.save_pkcs1()  # (format='PEM')
+                    privkey_pem = privkey.save_pkcs1()
+                    pub_key_file = open("pubkey.txt", "w")
+                    pub_key_file.write(str(pubkey_pem))
+                    priv_key_file = open("privkey.txt", "w")
+                    priv_key_file.write(str(privkey_pem))
+                    pub_key_file.close()
+                    priv_key_file.close()
+                    # Конец создания ключей
+
+                    SOCKET.send(pubkey_pem)
+                    server_pub_key_pem = SOCKET.recv(1024)
+                    server_pub_key = rsa.PublicKey.load_pkcs1(server_pub_key_pem, 'PEM')
                     data = 'r' + str(self.login_widget.text) + str(self.password1_widget.text)
-                    SOCKET.send(self.login_widget.text.encode('utf-8'))
-                    SOCKET.send(data.encode('utf-8'))
+                    secret_data = rsa.encrypt(data.encode('utf-8'), server_pub_key)
+                    secret_login = rsa.encrypt(self.login_widget.text.encode('utf-8'), server_pub_key)
+                    SOCKET.send(secret_login)
+                    SOCKET.send(secret_data)
                     threading.Thread(target=self.accept_handler).start()
                 else:
                     self.error_label_widget.text = 'Введённые пароли не совпадают'
@@ -79,7 +105,7 @@ class RegistrationScreen(Screen):
                                            'содержать не более 32 символов и не содержать спецсимволов'
 
     def accept_handler(self):
-        accept = str(SOCKET.recv(1024))[2:-1]
+        accept = SOCKET.recv(1024).decode("utf-8")
         if accept == 'True':
             self.parent.current = 'chat'
         else:

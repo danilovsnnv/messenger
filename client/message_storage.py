2dbf69176f2fd9c7cdc6fb2fe344f1009a32ab13
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-12-23 00:23:29 +0300

    Добавлена функция добавления непринятых сообщений

--- client/message_storage.py
+++ client/message_storage.py
@@ -13,34 +13,36 @@ class MessageStorage:
         Создание или чтение JSON файла, создание словаря
         """
         if not os.path.exists('messages.json'):
-            self.json_file = open("messages.json", "w")
-            self.messages_dict = {}
-            json.dump(self.messages_dict, self.json_file, indent=4)
+            with open("messages.json", "w") as self.json_file:
+                self.messages_dict = {}
+                json.dump(self.messages_dict, self.json_file, indent=4)
         else:
             with open('messages.json', 'r') as self.json_file:
                 self.messages_dict = json.load(self.json_file)
 
-    def add_message(self, chat: str, user: str, text: str):
+    def add_message(self, chat: str, user: str, text: str, time=datetime.datetime.now()):
         """
         Добавление сообщения в JSON файл
         :param chat: Название чата
         :param user: Имя пользователя, отправившего сообщение
         :param text: Текст сообщения
+        :param time: Время отправки сообщения
         """
-        self.messages_dict[chat].append([user, text, str(datetime.datetime.now())])
+        self.messages_dict[chat].append([user, text, str(time)])
         if len(self.messages_dict[chat]) > 100:
             del self.messages_dict[chat][0]
         with open("messages.json", "w") as json_file:
             json.dump(self.messages_dict, json_file, indent=4)
 
-    def add_message_in_new_chat(self, new_chat: str, user: str, text: str):
+    def add_message_in_new_chat(self, new_chat: str, user: str, text: str, time=datetime.datetime.now()):
         """
-            Создание нового чата и добавление сообщения в JSON файл
-            :param new_chat: Название нового чата
-            :param user: Имя пользователя, отправившего сообщение
-            :param text: Текст сообщения
+        Создание нового чата и добавление сообщения в JSON файл
+        :param new_chat: Название нового чата
+        :param user: Имя пользователя, отправившего сообщение
+        :param text: Текст сообщения
+        :param time: Время отправки
         """
-        self.messages_dict[new_chat] = [[user, text, str(datetime.datetime.now())]]
+        self.messages_dict[new_chat] = [[user, text, str(time)]]
         with open("messages.json", "w") as self.json_file:
             json.dump(self.messages_dict, self.json_file, indent=4)
 
@@ -69,3 +71,20 @@ class MessageStorage:
         :return: True или False в зависимости от того, найден ли чат
         """
         return username in list(self.messages_dict)
+
+    def update_storage(self, messages_list: str):
+        """
+        Добавляет в хранилище новые неполученные сообщения
+        :param messages_list: Строка с сообщениями
+        """
+        if messages_list == ['']:
+            return
+        while messages_list:
+            if self.contains_chat(messages_list[0]):
+                self.add_message(messages_list[0], messages_list[0], messages_list[1],
+                                 messages_list[2])
+            else:
+                self.add_message_in_new_chat(messages_list[0], messages_list[0], messages_list[1],
+                                             messages_list[2])
+
+            del messages_list[0:3]

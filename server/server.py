7b91895688e7609e75c72b8afb43900b1d3ca6e3
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-12-24 14:22:27 +0300

    Добавлено шифрование сообщений

--- server/server.py
+++ server/server.py
@@ -22,7 +22,7 @@ class Server:
 
     def connect(self):
         """
-        Подлючнеие новых пользователей
+        Подлючнеие новых пользователей, запускаемое из потока
         """
         while True:
             client, address = self.server.accept()
@@ -53,10 +53,9 @@ class Server:
         if accept:
             threading.Thread(target=self.message_handler, args=(client_socket,)).start()
             self.members_dict[username] = client_socket
-            # Отправка сообщений для синхронизации
-            self.send_messages(client_socket, username)
+            self.send_unreceived_messages(client_socket, username)
         else:
-            self.check_user_data(client_socket)
+            threading.Thread(target=self.check_user_data, args=(client_socket,)).start()
 
     def message_handler(self, client_socket: socket.socket):
         """
@@ -64,35 +63,42 @@ class Server:
         :param client_socket: Сокет отправителя
         """
         while True:
-            # Получение сообщения
-            message = client_socket.recv(1024).decode("utf-8").split('&', 3)[1::]
+            message1 = client_socket.recv(1024)
+            try:
+                [message, secret_message] = message1.split(sep="==%==".encode('utf-8'))
+            except ValueError:
+                message = message1
+            message = message.decode("utf-8")
+            message = message.split('&', 3)[1::]
+            if message[0] == "get_keys":
+                open_key_pem = database.get_open_key(message[1])
+                client_socket.send(("&key&" + open_key_pem.decode('utf-8')).encode('utf-8'))
+                continue
             if message[0] == 'search_users':
-                # Если это запрос поиска пользователей
                 self.search_users(client_socket, message[1])
                 continue
             try:
-                # Если пришло обычное сообщение
                 receiver_socket = self.members_dict[message[1]]
-                # Проверка на онлайн
                 online = self.online_check(receiver_socket)
                 if online:
-                    receiver_socket.send(('&' + message[0] + '&' + message[2]).encode('utf-8'))
-                    # Добавление сообщения в базу данных
-                    database.add_message(message[0], message[1], message[2], unreceived=False)
-                database.add_message(message[0], message[1], message[2], unreceived=True)
+                    receiver_socket.send(('&' + message[0] + '==%==').encode('utf-8') + secret_message)
+                    database.add_message(message[0], message[1], secret_message, unreceived=False)
+                else:
+                    database.add_message(message[0], message[1], secret_message, unreceived=True)
             except KeyError:
-                database.add_message(message[0], message[1], message[2], unreceived=True)
+                database.add_message(message[0], message[1], secret_message, unreceived=True)
+
             time.sleep(1)
 
     @staticmethod
-    def send_messages(client_socket: socket.socket, username: str):
+    def send_unreceived_messages(client_socket: socket.socket, username: str):
         """
         Поиск и отправка сообщений для синхронизации
         :param client_socket: Сокет клиента для синхронизации
         :param username: Имя пользователя для поиска сообщений
         """
         messages = database.get_messages(username)
-        client_socket.send(('&synchronize_messages&' + str(messages)).encode('utf-8'))
+        client_socket.send('&unreceived_messages&==!=='.encode('utf-8') + messages)
 
     @staticmethod
     def search_users(client_socket: socket.socket, username: str):

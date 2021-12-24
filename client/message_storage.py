267bfabc1b8c2d7a6e70b1f79ad775337719be36
Danil Ovsyannikov <70640782+yngghostt@users.noreply.github.com>
2021-12-24 03:36:19 +0300

    Переработаны функции добавления и обновления
    При синхронизации сообщений хранилище перезаписывается, объединены функции добавления сообщений, добавлена информация о том, прочитано ли сообщение

--- client/message_storage.py
+++ client/message_storage.py
@@ -1,5 +1,4 @@
 import json
-import os
 import datetime
 
 
@@ -12,39 +11,30 @@ class MessageStorage:
         """
         Создание или чтение JSON файла, создание словаря
         """
-        if not os.path.exists('messages.json'):
-            with open("messages.json", "w") as self.json_file:
-                self.messages_dict = {}
-                json.dump(self.messages_dict, self.json_file, indent=4)
-        else:
-            with open('messages.json', 'r') as self.json_file:
-                self.messages_dict = json.load(self.json_file)
+        with open("messages.json", "w") as self.json_file:
+            self.messages_dict = {}
+            json.dump({}, self.json_file, indent=4)
 
-    def add_message(self, chat: str, user: str, text: str, time=datetime.datetime.now()):
+    def save(self):
+        with open("messages.json", "w") as self.json_file:
+            json.dump(self.messages_dict, self.json_file, indent=4)
+
+    def add_message(self, chat: str, user: str, text: str, unread: bool, time=None):
         """
         Добавление сообщения в JSON файл
         :param chat: Название чата
         :param user: Имя пользователя, отправившего сообщение
         :param text: Текст сообщения
+        :param unread: Прочитано ли сообщение
         :param time: Время отправки сообщения
         """
-        self.messages_dict[chat].append([user, text, str(time)])
-        if len(self.messages_dict[chat]) > 100:
-            del self.messages_dict[chat][0]
-        with open("messages.json", "w") as json_file:
-            json.dump(self.messages_dict, json_file, indent=4)
-
-    def add_message_in_new_chat(self, new_chat: str, user: str, text: str, time=datetime.datetime.now()):
-        """
-        Создание нового чата и добавление сообщения в JSON файл
-        :param new_chat: Название нового чата
-        :param user: Имя пользователя, отправившего сообщение
-        :param text: Текст сообщения
-        :param time: Время отправки
-        """
-        self.messages_dict[new_chat] = [[user, text, str(time)]]
-        with open("messages.json", "w") as self.json_file:
-            json.dump(self.messages_dict, self.json_file, indent=4)
+        if time is None:
+            time = datetime.datetime.now()
+        try:
+            self.messages_dict[chat].append([user, text, str(time), unread])
+        except KeyError:
+            self.messages_dict[chat] = [[user, text, str(time), True]]
+        self.save()
 
     def get_messages(self, username: str) -> dict:
         """
@@ -53,6 +43,10 @@ class MessageStorage:
         :return: Список с сообщениями из чата
         """
         try:
+            for i in range(len(self.messages_dict[username])):
+                if self.messages_dict[username][i][-1]:
+                    self.messages_dict[username][i][-1] = False
+            self.save()
             return self.messages_dict[username]
         except KeyError:
             return []
@@ -62,7 +56,7 @@ class MessageStorage:
         Возвращает список пользователей, с которыми есть переписка
         :return: Список пользователей, с которыми начат чат
         """
-        return list(self.messages_dict)
+        return list(dict(sorted(self.messages_dict.items(), key=lambda x: x[1][-1][-2], reverse=True)))
 
     def contains_chat(self, username):
         """
@@ -72,19 +66,22 @@ class MessageStorage:
         """
         return username in list(self.messages_dict)
 
+    def has_unread(self, username):
+        """
+        Проверяет наличие непрочитанных сообщений в чате с пользователем
+        :param username: Имя пользователя
+        :return: Есть ли непрочитанные сообщения
+        """
+        return True in sum(self.messages_dict[username], [])
+
     def update_storage(self, messages: list):
         """
-        Добавляет в хранилище новые неполученные сообщения
+        Обновляет хранилище сообщениями, полученными с сервера
         :param messages: Список с сообщениями
         """
         if messages == ['']:
             return
+        self.messages_dict.clear()
         while messages:
-            if self.contains_chat(messages[0]):
-                self.add_message(messages[0], messages[0], messages[1],
-                                 messages[2])
-            else:
-                self.add_message_in_new_chat(messages[0], messages[0], messages[1],
-                                             messages[2])
-
-            del messages[0:3]
+            self.add_message(messages[0], messages[0], messages[1], messages[2], messages[3])
+            del messages[0:4]
